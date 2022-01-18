8feaeae113ad46539dd419b3cf199f71457745bf
Sechang OH <twinf1004@naver.com>
2022-01-18 22:09:32 +0900

    연산자와 조건
    오늘은 자바의 연산자와 조건에 대하여 알아보았습니다

---
+++ 연산자와 조건.java
@@ -0,0 +1,20 @@
+// 대입, 산술, 축약 대입 연산자
+
+/*import java.io.*;
+
+public class Main{
+	public static void main(String[] args){
+		
+		int day, remainder;
+		day = remainder = 365;
+		int week = 365 / 7;
+		
+		System.out.format("1년은 %d일이며 %d주이다. %n", day, week); // 여기 format의 역할이 무엇인 모르겠다....
+		System.out.println(remainder %= 7);
+		System.out.println(remainder);
+		
+	}
+}*/
+
+//증가와 감소 연산자
+
